a2f47ce9ce02055ca2722ce5feaa50ed1977c216
Connor Fisher <connorfisher@Connors-Mac-mini.local>
2024-01-09 19:12:28 +1000

    List Functionality

--- user_list/base/models.py
+++ user_list/base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class List(models.Model):
+    item = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.item)
+        
--- user_list/base/views.py
+++ user_list/base/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import List
 # Create your views here.
+
+def index(request):
+    items = List.objects.all()
+    if request.method == 'POST':
+        new_item = request.POST.get("add_item")
+        List.objects.create(item=new_item)
+
+    return render(request, 'face/index.html',{"items": items})
+
+def delete(request):
+    items = List.objects.all()
+    if request.method == 'POST':
+        delete_item = request.POST.get("delete_item")
+        List.objects.filter(item=delete_item).delete()
+    return render(request, 'face/delete.html',{"items":items})
